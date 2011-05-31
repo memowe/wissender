0eea0545f070494b1f43f3bb8ae50660686b3775
Mirko Westermeier <mirko@westermeier.de>
2011-05-31 16:02:33 +0200

    added the utf8 pragma for non-ascii chars in code

--- wissender.pl
+++ wissender.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use utf8;
 use Getopt::Std;
 
 package Wissender;
@@ -59,7 +60,7 @@ my %opt;
 getopt('hdspcnur', \%opt);
 
 # help message
-if ($opt{h} or not keys %opt) { print <<'EOF'; exit }
+if ($opt{h} or not keys %opt) { print <<EOF; exit }
 
 WISSENDER
 
